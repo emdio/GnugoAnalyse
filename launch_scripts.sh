2d2d531af67b8a3189441aa6ba873203931c9e3d
emdio <uc3709@yahoo.es>
2014-09-14 22:46:12 +0200

    rmeove launch_script.sh file

--- launch_scripts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PATH=/usr/bin:/bin:/sbin:/usr/games/
-
-cd ~/soft/GoAnalyser
-./downloadSgfAttached.py
-./analyseSgfFiles.py > /home/emilio/ipsender.log 2>&1
-#./analyseSgfFiles.py
-./sendGmailAttachment.py
